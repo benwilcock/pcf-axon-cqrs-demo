bb4d41584bfa44ba56f3a5bcb4fc205c3cb76623
benwilcock <benwilcock@gmail.com>
2017-05-18 14:50:20 +0100

    Updated the build pipeline to use the /dash endpoint. Added EnableDiscoveryClient to swich on registration.

--- command-side/src/main/java/io/pivotal/PcfSpringBootApplication.java
+++ command-side/src/main/java/io/pivotal/PcfSpringBootApplication.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.HashMap;
 import java.util.Map;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PcfSpringBootApplication {
 
