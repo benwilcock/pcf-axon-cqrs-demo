ad7cf82e0219741271386a8bc1d65ce7d67bd6d8
benwilcock <benwilcock@gmail.com>
2017-07-06 15:55:06 +0100

    cleaned up the services scripts

--- pws-create-pcf-services.sh
+++ pws-create-pcf-services.sh
@@ -2,6 +2,5 @@
 
 cf create-service cleardb spark mysql
 cf create-service cloudamqp lemur rabbit
-cf create-service xxxx xxxx redis
 cf create-service p-service-registry standard registry
 cf create-service p-config-server standard config -c config-server-setup.json
